1aaa310b26d2c74a7cc9ac3aac6ea99f0da9ac4a
Bob <petrbobe@seznam.cz>
2024-06-10 15:38:23 +0200

    weather api

--- src/index.js
+++ src/index.js
@@ -8,4 +8,34 @@ window.onload = () => {
   }
 
 const apiKey = "cda4c46d1d07ecfb7a91c6d9b946210c";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchInput = document.querySelector(".card__header input");
+const searchBtn = document.querySelector(".card__header button");
+const weatherIco = document.querySelector(".card__body > img");
+
+async function checkWeather(city) {
+  const res = await fetch(apiURL + city + `&appid=${apiKey}`);
+  
+  if (res.status == 404) {
+    document.querySelector(".card__error").style.display = "block";
+    document.querySelector(".card__body").style.display = "none";
+    return;
+  }
+
+  let data = await res.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  weatherIco.src = `images/${data.weather[0].main}.png`
+  document.querySelector(".card__body").style.display = "block";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchInput.value);
+});
